8571b207c35d2e6d1c35b5f8f6b793d5e4cf4c33
erikwag <erikwag@WS-VGY-1951>
2016-12-02 14:33:09 +0100

    Add project files.

---
+++ samlingsklasser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samlingsklasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
